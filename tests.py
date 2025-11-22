6b7b7112ad64c5ba4b3e14690fffde62dc51d109
David Durost <david.durost@gmail.com>
2025-11-22 07:19:25 -0500

    Cover faculty class assignment views

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 from unittest.mock import patch
+from datetime import time
 
 from django.test import RequestFactory
 from django.http import Http404
@@ -10,6 +11,18 @@ from .models.faculty import FacultyProfile
 from .models.quarters import Quarters, QuartersType
 from .forms.quarters import QuartersForm
 from .views.faculty import ManageView
+from facility.models.department import Department
+from enrollment.models.faculty import FacultyEnrollment as FacultyEnrollmentRecord
+from enrollment.models.facility_class import (
+    FacilityClassEnrollment as FacilityClassSchedule,
+)
+from enrollment.models.faculty_class import FacultyClassEnrollment as FacultyClassAssignment  # noqa: F401
+from enrollment.models.temporal import Week, Period
+from course.models.facility_class import FacilityClass
+from enrollment.views.facility import (
+    FacultyClassEnrollmentCreateView,
+    FacultyClassEnrollmentUpdateView,
+)
 from enrollment.models.faculty import FacultyEnrollment as FacultyEnrollmentRecord
 from enrollment.views.facility import (
     FacultyEnrollmentCreateView,
@@ -193,5 +206,140 @@ class FacultyEnrollmentViewTests(BaseDomainTestCase):
             quarters=self.quarters,
             role="Lead",
             instance=existing,
+        )
+        self.assertEqual(response.status_code, 302)
+
+
+class FacultyClassAssignmentViewTests(BaseDomainTestCase):
+    def setUp(self):
+        super().setUp()
+        self.factory = RequestFactory()
+        with mute_profile_signals():
+            self.user = User.objects.create_user(
+                username="faculty.coordinator2",
+                password="pass12345",
+                user_type=User.UserType.FACULTY,
+                is_admin=True,
+            )
+        self.profile = FacultyProfile.objects.create(
+            user=self.user,
+            organization=self.organization,
+            facility=self.facility,
+        )
+        self.quarters_type = QuartersType.objects.create(
+            name="Faculty Cabin B",
+            organization=self.organization,
+        )
+        self.quarters = Quarters.objects.create(
+            name="Cabin Echo",
+            facility=self.facility,
+            type=self.quarters_type,
+            capacity=2,
+        )
+        self.faculty_enrollment = FacultyEnrollmentRecord.objects.create(
+            name="Summer Faculty",
+            faculty=self.profile,
+            facility_enrollment=self.facility_enrollment,
+            quarters=self.quarters,
+        )
+        self.department = Department.objects.create(
+            name="Program Dept",
+            abbreviation="PRG",
+            facility=self.facility,
+        )
+        self.week = Week.objects.create(
+            name="Week A",
+            start=self.facility_enrollment.start,
+            end=self.facility_enrollment.end,
+            facility_enrollment=self.facility_enrollment,
+        )
+        self.period = Period.objects.create(
+            name="Morning",
+            start=time(8, 0),
+            end=time(9, 0),
+            week=self.week,
+        )
+        self.facility_class = FacilityClass.objects.create(
+            name="First Aid Skills",
+            organization_course=self.org_course,
+            facility_enrollment=self.facility_enrollment,
+        )
+        self.class_enrollment = FacilityClassSchedule.objects.create(
+            facility_class=self.facility_class,
+            period=self.period,
+            department=self.department,
+            organization_enrollment=self.org_enrollment,
+            max_enrollment=20,
+        )
+
+    def _build_request(self):
+        request = self.factory.post("/facilities/faculty/class-enrollments/new/")
+        request.user = self.user
+        return request
+
+    def _build_form(self):
+        return SimpleNamespace(
+            cleaned_data={
+                "faculty": self.profile,
+                "facility_class_enrollment": self.class_enrollment,
+                "faculty_enrollment": self.faculty_enrollment,
+            },
+            add_error=lambda *args, **kwargs: None,
+        )
+
+    def test_faculty_class_assignment_create_uses_service(self):
+        view = FacultyClassEnrollmentCreateView()
+        view.request = self._build_request()
+        view.kwargs = {
+            "facility_slug": self.facility.slug,
+            "faculty_slug": self.profile.slug,
+            "faculty_enrollment_slug": self.faculty_enrollment.slug,
+        }
+        view.get_success_url = lambda: "/next/"
+        form = self._build_form()
+
+        with patch.object(
+            FacultyClassEnrollmentCreateView, "service_class"
+        ) as service_cls:
+            service_instance = service_cls.return_value
+            service_instance.assign_faculty_to_class.return_value = SimpleNamespace(
+                pk=10
+            )
+            response = view.form_valid(form)
+
+        service_instance.assign_faculty_to_class.assert_called_once_with(
+            faculty=self.profile,
+            facility_class_enrollment=self.class_enrollment,
+            faculty_enrollment=self.faculty_enrollment,
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_faculty_class_assignment_update_passes_instance(self):
+        existing = SimpleNamespace(pk=99)
+        view = FacultyClassEnrollmentUpdateView()
+        view.request = self._build_request()
+        view.kwargs = {
+            "facility_slug": self.facility.slug,
+            "faculty_slug": self.profile.slug,
+            "faculty_enrollment_slug": self.faculty_enrollment.slug,
+        }
+        view.get_object = lambda: existing
+        view.get_success_url = lambda: "/next/"
+        form = self._build_form()
+
+        with patch.object(
+            FacultyClassEnrollmentUpdateView, "service_class"
+        ) as service_cls:
+            service_instance = service_cls.return_value
+            service_instance.assign_faculty_to_class.return_value = SimpleNamespace(
+                pk=11
+            )
+            response = view.form_valid(form)
+
+        service_instance.assign_faculty_to_class.assert_called_once_with(
+            faculty=self.profile,
+            facility_class_enrollment=self.class_enrollment,
+            faculty_enrollment=self.faculty_enrollment,
+            assignment=existing,
         )
         self.assertEqual(response.status_code, 302)
